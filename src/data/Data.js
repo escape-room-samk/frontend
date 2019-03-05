8e3d0d3c340b9696ca9893b86f0abf02b6c42dcb
sybren-marechal <sybrenmarechal@icloud.com>
2019-03-05 17:37:51 +0200

    testing

---
+++ src/data/Data.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const BACKEND_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+
+function getData(api) {
+  axios
+    .get(BACKEND_URL + api)
+    .then(response => {
+      // create an array of data only with relevant data
+      const newData = response.data.map(c => {
+        return c;
+      });
+      const newState = Object.assign({}, this.state, {
+        data: newData
+      });
+      this.setState(newState);
+    })
+    .catch(error => console.log(error));
+}
+
+function getImage() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(BACKEND_URL + "/api/imageReader")
+      .then(results => {
+        const image = results.data.map(element => {
+          return element;
+        });
+        resolve(image);
+      })
+      .catch(error => {
+        console.log(error);
+        reject();
+      });
+  });
+}
+
+export default {getData,getImage};
