9a4c377ad5a2bd03c932c08898321c57659da284
sybren-marechal <sybrenmarechal@icloud.com>
2019-03-07 16:00:07 +0200

    imageSensorpage

--- src/imageSensor/imageSensor.js
+++ src/imageSensor/imageSensor.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import DataList from "../imageSensor/DataList";
+
 import logo from "../logo.svg";
+import axios from "axios";
+
 // import DataList from "../components/DataList";
 class imageSensor extends Component {
+  state = {
+    dataImage: []
+
+  };
+  componentDidMount() {
+
+    axios
+      .get("http://172.17.2.10:3000/api/imageReader")
+      .then(response => {
+        // create an array of data only with relevant data
+        const newData = response.data.map(c => {
+          return {
+            devID: c.devID,
+            question: c.question,
+            boolean: c.boolean,
+            value: c.value
+          };
+        });
+        const newState = Object.assign({}, this.state, {
+          dataImage: newData
+            .splice(newData.length - 5, newData.length - 1)
+            .reverse()
+        });
+        this.setState(newState);
+      })
+      .catch(error => console.log(error));
+    }
+
+
   render() {
     return (
       <div className="App">
@@ -12,10 +45,11 @@ class imageSensor extends Component {
           <h1 className="App-title">React Image Reader</h1>
         </header>
         <Container>
+
           <Row>
             <Col>
               {"IMAGE sensor"}
-              {/* <DataList data={this.state.dataImage} /> */}
+              <DataList data={this.state.dataImage} />
             </Col>
           </Row>
         </Container>
