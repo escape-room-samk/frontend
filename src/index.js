cb2c6b3e6b1aaf24e8bef75fbc3459ec4c7f8c96
sybren-marechal <sybrenmarechal@icloud.com>
2019-03-08 14:34:18 +0200

    add routes for codes

--- src/index.js
+++ src/index.js
@@ -5,16 +5,34 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { Route,BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import imageSensor from './imageSensor/imageSensor'
 import login from './login/login'
+import code_1 from './code_1/code_1'
+import code_2 from './code_2/code_2'
+import code_3 from './code_3/code_3'
+import code_complete from './code_complete/code_complete'
+import code_overvieuw from './code_overvieuw/code_overvieuw'
+
+
+
+
 
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={App} />
-      <Route path="/imageSensor" component={imageSensor} />
       <Route path="/login" component={login} />
+      <Route path="/code_1" component={code_1} />
+      <Route path="/code_2" component={code_2} />
+      <Route path="/code_3" component={code_3} />
+      <Route path="/code_complete" component={code_complete} />
+      <Route path="/code_overvieuw" component={code_overvieuw} />
+
+      
+
+
+
+
 
     </div>
   </Router>
