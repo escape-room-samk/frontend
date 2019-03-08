612fed99e1a60d9b1251206240bceed68b774463
sybren-marechal <sybrenmarechal@icloud.com>
2019-03-08 11:39:00 +0200

    add login form

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import './index.css'
 import { Route,BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import imageSensor from './imageSensor/imageSensor'
+import login from './login/login'
+
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={App} />
       <Route path="/imageSensor" component={imageSensor} />
+      <Route path="/login" component={login} />
+
     </div>
   </Router>
 )
