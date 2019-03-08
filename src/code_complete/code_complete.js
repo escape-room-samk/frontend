db17fbc0e330a19d1a19382f62c008bc4c718498
sybren-marechal <sybrenmarechal@icloud.com>
2019-03-08 14:35:17 +0200

    add pages

---
+++ src/code_complete/code_complete.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../logo.svg";
+
+// import DataList from "../components/DataList";
+class RfidReader extends Component {
+  state = {
+    dataImage: []
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ESCAPE ROOM</h1>
+        </header>
+        <Container>
+        <h1>COMPLETE</h1>
+        </Container>
+      </div>
+    );
+  }
+}
+export default RfidReader;
